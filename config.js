5721f15476e797a678937491030d1224904790c0
ogohogo <78665139+ogohogo@users.noreply.github.com>
2022-09-21 17:15:42 +0200

    well perhaps dont use my own proxy lmao

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     proxy: {
-        enabled: true, //Should we use proxies? Can be either "true" or "false"
-        proxy: 'http://p.webshare.io:9999' //either "format://host:port" OR "format://username:password@host:port"
+        enabled: false, //Should we use proxies? Can be either "true" or "false"
+        proxy: '' //either "format://host:port" OR "format://username:password@host:port"
     },
     delayBetweenRequests: 100, //Delay between each request, in miliseconds!
     requestTimeout: 5
